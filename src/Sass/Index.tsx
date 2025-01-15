6f0511265a120a2c34591a7798880d3ee0040450
isha-tag97 <isha@tag97.com>
2025-01-15 15:33:15 +0530

    msg:add chnages

--- src/Sass/Index.tsx
+++ src/Sass/Index.tsx
@@ -6,8 +6,9 @@ function Index() {
   return (
     <div className='container'>
         <h5 className='title'>Heyyyy</h5>
+        <button className="hover-btn">Hover</button>
+
     </div>
   )
 }
-
 export default Index
